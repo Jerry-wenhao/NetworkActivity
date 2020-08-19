001e2431067b6f6d8fbe3dcdb7e6979dc4bdbde6
Jerry777777 <491818175@qq.com>
2020-08-20 03:20:21 +0800

    [xiawenhao] feat: add a counter to count app onStart times

--- app/src/main/java/com/xiawenhao/networkactivity/MainActivity.java
+++ app/src/main/java/com/xiawenhao/networkactivity/MainActivity.java
@@ -2,6 +2,9 @@ package com.xiawenhao.networkactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.net.Network;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -26,15 +29,28 @@ import okhttp3.Response;
 public class MainActivity extends AppCompatActivity {
     @BindView(R.id.button)
     Button buttonGetMessage;
+    @BindView(R.id.count_button)
+    Button CountNumber;
 
     String url = "https://twc-android-bootcamp.github.io/fake-data/data/default.json";
 
     Gson gson = new Gson();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        CountNumber.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences sharedPref = MainActivity.this.getPreferences(Context.MODE_PRIVATE);
+                int countNumber = sharedPref.getInt("count", 0);
+                Toast.makeText(getApplicationContext(), Integer.toString(countNumber), Toast.LENGTH_SHORT).show();
+            }
+        });
+
 
         buttonGetMessage.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -68,6 +84,15 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        SharedPreferences sharedPref = MainActivity.this.getPreferences(Context.MODE_PRIVATE);
+        int count = sharedPref.getInt("count", 0);
+        sharedPref.edit().putInt("count", ++count).apply();
+    }
+
 
     private void dataHandler(String message) {
         Wrapper wrapper = gson.fromJson(message, Wrapper.class);
